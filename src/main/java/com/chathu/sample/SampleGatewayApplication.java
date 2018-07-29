2a12e0b35d0a079087ba9d0668e375deb3e93223
ChathushkaHansani <ChathushkaHansani@gmail.com>
2018-07-30 02:38:37 +0530

    initial commit

---
+++ src/main/java/com/chathu/sample/SampleGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.chathu.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class SampleGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SampleGatewayApplication.class, args);
+    }
+}
